446d1302eb8aebb8725b917aa8ddd895aad430c1
Robert Wagner <rwagner@jibe.com>
2016-06-16 15:03:54 -0400

    Add normalizeEntityName

--- blueprints/ember-newton-cradle-loader/index.js
+++ blueprints/ember-newton-cradle-loader/index.js
@@ -1,6 +1,12 @@
 /*jshint node:true*/
 module.exports = {
-  description: 'Creates a loading.hbs'
+  description: 'Creates a loading.hbs',
+
+  normalizeEntityName: function () {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  }
 
   // locals: function(options) {
   //   // Return custom template variables here.
