d97a72d91cb655d90754d92261129371763785dc
Robert Wagner <rwwagner@vt.edu>
2016-06-16 21:18:01 -0400

    Add loading demo, fix blueprint

---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+const {Route, RSVP, run} = Ember;
+
+export default Route.extend({
+  model() {
+    return new RSVP.Promise((resolve) => {
+      run.later(() => {
+        resolve({});
+      }, 3000);
+    });
+  }
+});
