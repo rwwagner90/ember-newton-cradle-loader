407d0dbdf4c6d29c5b38fe40f1af0b9a58b2ea69
Robert Wagner <rwwagner@vt.edu>
2016-08-14 20:39:49 -0400

    Add meaningful tests

--- tests/integration/components/newton-cradle-loader/component-test.js
+++ tests/integration/components/newton-cradle-loader/component-test.js
@@ -1,25 +1,45 @@
 import { moduleForComponent, test } from 'ember-qunit';
-// import hbs from 'htmlbars-inline-precompile';
+import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('newton-cradle-loader', 'Integration | Component | newton cradle loader', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(0);
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('newton cradle renders', function(assert) {
+  assert.expect(2);
 
-  // this.render(hbs`{{newton-cradle-loader}}`);
-  //
-  // assert.equal(this.$().text().trim(), '');
-  //
-  // // Template block usage:
-  // this.render(hbs`
-  //   {{#newton-cradle-loader}}
-  //     template block text
-  //   {{/newton-cradle-loader}}
-  // `);
-  //
-  // assert.equal(this.$().text().trim(), 'template block text');
+  this.render(hbs`{{newton-cradle-loader}}`);
+
+  assert.ok(this.$('.swing').is(':visible'));
+  assert.ok(this.$('.shadow').is(':visible'));
+});
+
+test('swing html', function(assert) {
+  assert.expect(1);
+
+  this.render(hbs`{{newton-cradle-loader}}`);
+
+  assert.equal(this.$('.swing').html().trim(),
+  `<div class="swing-l"></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div class="swing-r"></div>`);
+});
+
+test('shadow html', function(assert) {
+  assert.expect(1);
+
+  this.render(hbs`{{newton-cradle-loader}}`);
+
+  assert.equal(this.$('.shadow').html().trim(),
+  `<div class="shadow-l"></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div class="shadow-r"></div>`);
 });
