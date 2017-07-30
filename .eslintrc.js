8842b2e85b223cfed8ac2e443814a942e99c9a87
Robert Wagner <rwwagner90@gmail.com>
2017-07-30 17:01:40 -0400

    Update ember

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:ship-shape/recommended'
-  ],
+  extends: 'eslint:recommended',
   env: {
     browser: true
   },
--- blueprints/ember-newton-cradle-loader/index.js
+++ blueprints/ember-newton-cradle-loader/index.js
@@ -7,15 +7,4 @@ module.exports = {
     // not specified (since that doesn't actually matter
     // to us
   }
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
 };
