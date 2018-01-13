ae92bd373234526453770e92fdf332115a84fe6a
Robert Wagner <rwwagner90@gmail.com>
2018-01-13 10:18:00 -0500

    Ember 3.0 beta, bump deps

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,5 +1,6 @@
-import Ember from 'ember';
-const { Route, RSVP, run } = Ember;
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import { run } from '@ember/runloop';
 
 export default Route.extend({
   model() {
